0ab08de4d538e5c8d23f0c6ddebf7a6822628238
Nicolas Brondin-Bernard <nicolas@brondin.com>
2020-05-11 15:01:57 +0200

    [CONTENT] Added the newsletter page

---
+++ pages/newsletter.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="page-content">
+        <section>
+            <flag text="De junior à warrior"/>
+            <h1>La newsletter pour faire ressortir le meilleur du jeune développeur web qui est en toi.</h1>
+            <p>Deviens membre de la <strong>newsletter</strong> et reçois un email <strong>tous les mardis à 8h00</strong>. Je te partage <strong>mes conseils</strong>  (tech, recrutement, soft skills, gestion de projet,...) <strong>pour t'aider à progresser</strong> !</p>
+            <p>Je te parlerai de tout, en toute transparence, <strong>sans bullshit</strong> !</p>
+            <div id="gift-container">
+                <img src="@/assets/img/icons/gift.svg" id="gift" @click="show_love" v-if="!love" height="50"/>
+                <img src="@/assets/img/icons/love.svg" id="heart" v-if="love" height="50"/>
+            </div>
+            <a class="btn primary" href="http://n2b.link/newsletter" target="_blank">Devenir membre<img src="@/assets/img/icons/crown.svg" height="20"/></a>
+            <p class="newsletter-price">pour <strong>2€/mois</strong>, soit le prix de 4 cafés !</p>
+        </section>
+        
+        
+        
+    </div>
+</template>
+
+<script>
+
+
+    import flag from "../components/flag.vue"
+    
+    export default {
+        head: function(){
+            return {
+                title: this.$t("META_INDEX_TITLE"),
+                meta: [
+                    { hid: "twitter:title", name: "twitter:title", content: "De junior à warrior : la newsletter pour les nouveaux développeurs web"},
+                    { hid: "twitter:text:title", name: "twitter:text:title", content: "De junior à warrior : la newsletter pour les nouveaux développeurs web"},
+                    { hid: "og:title", name: "og:title", content: "De junior à warrior : la newsletter pour les nouveaux développeurs web"},
+                    { hid: "description", name: "description", content: "Deviens membre de la newsletter et reçois un email tous les mardis à 8h00. Je te partage mes conseils (tech, recrutement, soft skills, gestion de projet,...) pour t'aider à progresser !"},
+                    { hid: "og:description", name: "og:description", content: "Deviens membre de la newsletter et reçois un email tous les mardis à 8h00. Je te partage mes conseils (tech, recrutement, soft skills, gestion de projet,...) pour t'aider à progresser !"},
+                    { hid: "twitter:description", name: "twitter:description", content: "Deviens membre de la newsletter et reçois un email tous les mardis à 8h00. Je te partage mes conseils (tech, recrutement, soft skills, gestion de projet,...) pour t'aider à progresser !"},
+                    { hid: "twitter:image", name: "twitter:image", content: "https://nicolas.brondin-bernard.com/cover-newsletter.jpg?t=1" },
+                    { hid: "og:image", name: "og:image", content: "https://nicolas.brondin-bernard.com/cover-newsletter.jpg?t=1" }
+                ]
+            };
+        },
+        data: function(){
+            return {
+                love: false
+            };
+        },
+        components: {
+            "flag": flag
+        },
+        methods: {
+            show_love: function(){
+                this.love = true;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+@keyframes giggle{
+        0% { transform: rotate(0deg)}
+        10% { transform: rotate(5deg)}
+        20% { transform: rotate(-5deg)}
+        30% { transform: rotate(0deg)}
+        99% { transform: rotate(0deg)}
+    }
+@keyframes beat{
+        0% { transform: scale(1.0)}
+        10% { transform: scale(0.9)}
+        20% { transform: scale(1.1)}
+        30% { transform: scale(1.0)}
+        99% { transform: scale(1.0)}
+    }
+    #gift-container {
+        margin-bottom: 10px;
+    }
+    #gift {
+        cursor: pointer;
+        animation: giggle 2s ease-in-out 1s infinite;
+    }
+    #heart {
+        animation: beat 1s ease-in-out 1s infinite;
+    }
+    p.newsletter-price {
+        font-size: 14px;
+        margin: 5px 0px;
+        margin-bottom: 20px;
+        color: rgba(0,0,0,0.5);
+    }
+</style>
